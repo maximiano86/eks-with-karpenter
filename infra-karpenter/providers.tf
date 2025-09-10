3b76c85f374d71986648ecf985f3ad6871aaac7b
Max Power <57537575+maximiano86@users.noreply.github.com>
2025-09-10 15:05:48 -0500

    chore(terraform): cleanup code for Karpenter demo (#3)

--- infra-karpenter/providers.tf
+++ infra-karpenter/providers.tf
@@ -38,9 +38,3 @@ provider "helm" {
     }
   }
 }
-
-provider "kubernetes" {
-  host                   = module.eks_cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
